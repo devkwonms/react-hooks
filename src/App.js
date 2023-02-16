61bb5ac2598cea856a5d58c55bb280b458c25d74
devkwonms <alstj1379@nate.com>
2023-02-17 01:02:28 +0900

    onTab

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import logo from './logo.svg';
+import './App.css';
 
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Section 1"
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Section 2"
-  }
-];
-
-const useTab = (initialTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
-
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex
-  };
-};
-export default function App() {
-  const { currentItem, changeItem } = useTab(0, content);
+function App() {
   return (
     <div className="App">
-      {content.map((section, index) => (
-        <button key={index} onClick={() => changeItem(index)}>
-          {section.tab}
-        </button>
-      ))}
-      <div>{currentItem.content}</div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>Hello~</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
