f6c9cf874cdf9179d5792f8bd55a24cac81a0f83
devkwonms <alstj1379@nate.com>
2023-02-17 03:59:04 +0900

    sample

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -19,3 +19,15 @@ const onChange = (event) => {
 };
 return { value, onChange };
 };
+// sample
+// export default function App() {
+//   // const maxLen = (value) => value.length <= 10;
+// const maxLen = (value) => !value.includes("@");
+// const name = useInput("minseo", maxLen);
+// return (
+//     <div className="App">
+//     <h1>Hello</h1>
+//     <input placeholder="Name" {...name} />
+//     </div>
+// );
+// }
