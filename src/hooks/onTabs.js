beaa487cbd345600bc43c47f17bdbaa6c7f3436e
devkwonms <alstj1379@nate.com>
2023-02-17 01:14:13 +0900

    onTab

---
+++ src/hooks/onTabs.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const content = [
+{
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+},
+{
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+}
+];
+
+const useTab = (initialTab, allTabs) => {
+const [currentIndex, setCurrentIndex] = useState(initialTab);
+if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+}
+
+return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+};
+};
+export default function App() {
+const { currentItem, changeItem } = useTab(0, content);
+return (
+    <div className="App">
+    {content.map((section, index) => (
+        <button key={index} onClick={() => changeItem(index)}>
+        {section.tab}
+        </button>
+    ))}
+    <div>{currentItem.content}</div>
+    </div>
+);
+}
