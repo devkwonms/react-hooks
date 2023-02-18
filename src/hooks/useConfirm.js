0318fb406dac927b0c3284eccfc5d2ff96f9f186
devkwonms <alstj1379@nate.com>
2023-02-18 17:56:01 +0900

    변수명, 조건수정

--- src/hooks/useConfirm.js
+++ src/hooks/useConfirm.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 
-export const useConfirm = (message = "", callback, rejection) => {
-    if (typeof callback !== "function") {
+export const useConfirm = (message = "", onConfirm, onCancel) => {
+    if (!onConfirm || typeof callback !== "function") {
     return;
     }
+    if (onCancel && typeof onCancel !== "function") {
+        return;
+        }
     const confirmAction = () => {
     if (window.confirm(message)) {
-        callback();
+        onConfirm();
     } else {
-        rejection();
+        onCancel();
     }
     };
     return confirmAction;
