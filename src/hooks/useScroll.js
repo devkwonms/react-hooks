2d0383583a7990ff19c9236be5e4ecb1cc6647e1
devkwonms <alstj1379@nate.com>
2023-02-20 20:08:41 +0900

    useScroll

---
+++ src/hooks/useScroll.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState, useRef } from "react";
+import ReactDOM from "react-dom";
+
+export const useScroll = () => {
+    const [state, setState] = useState({
+    x: 0,
+    y: 0
+    });
+    const onScroll = () => {
+    setState({ y: window.scrollY, x: window.scrollX });
+    };
+    useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+return state;
+};
+
+// sample
+
+// export default function App() {
+//     const { y } = useScroll();
+//     return (
+//     <div className="App" style={{ height: "1000vh" }}>
+//         <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
+//         Hello
+//         </h1>
+//     </div>
+//     );
+// }
